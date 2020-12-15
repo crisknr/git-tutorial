cab0ffd4a42fb7f6e20548633724d1a6da311b8b
Alex Hoffman <alxhoff@gmail.com>
2020-12-15 22:13:34 +0100

    Coding Challenge 2

--- src/main.c
+++ src/main.c
@@ -1,7 +1,21 @@
 #include<stdio.h>
+#include <string.h>
+
+#include "espl_lib.h"
 
 int main() {
-	printf("Hello ESPL");
-	printf("This is a new print");
+	printf("Hello ESPL\n");
+	printf("This is a new print\n");
+	unsigned int num;
+	char state = 'y';
+	char words[25];
+	while (state == 'y'){
+		printf("Enter a number: ");
+		scanf(" %d", &num);
+		strcpy(words, num_to_words(num));
+		printf("Result: %s\n", words);
+		printf("Convert new number? [y]es / [n]o: ");
+		scanf(" %c", &state);
+	}
 	return 0;
 }
